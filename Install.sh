d91b67f1ca2ba67d53659460a2b5cefde42bbd1b
A.Bondar <66157526+EKXXai@users.noreply.github.com>
2025-09-02 08:29:47 -0400

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -6,5 +6,5 @@ mkdir projets
 
 sudo pacman -Sy git vim gcc gdb python3 python3-pip python3-venv
 
-cp dotfiles_tmp ~/.bashrc
-cp dotfiles_tmp ~/.config
+cp dotfiles_tmp/.bashrc ~/.bashrc
+cp dotfiles_tmp/ ~/.config
